09fb4e8ad614d1e90cdc8e428e6d0a0c4d1647b0
Jo Sunhyeon <35026790+josunhyeon@users.noreply.github.com>
2022-04-11 06:07:32 +0900

    Add initial files.

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private void Awake()
+    {
+        finiteStateMachine = gameObject.AddComponent<FiniteStateMachineRunner>().Initialize<State>(this, State.Idle);
+    }
+
+    private IEnumerator OnAttackStateEnter()
+    {
+        Debug.Log($"{GetType()}.{nameof(OnAttackStateEnter)}");
+        while (true)
+        {
+            yield return null;
+            Debug.Log($"Attack");
+        }
+    }
+
+    private void OnIdleStateEnter()
+    {
+        Debug.Log($"{GetType()}.{MethodBase.GetCurrentMethod().Name}");
+    }
+
+    private void OnIdleStateUpdate()
+    {
+        Debug.Log($"{GetType()}.{MethodBase.GetCurrentMethod().Name}");
+        if (Time.time > 1)
+        {
+            finiteStateMachine.State = State.Attack;
+        }
+    }
+
+    private void OnIdleStateExit()
+    {
+        Debug.Log($"{GetType()}.{MethodBase.GetCurrentMethod().Name}");
+    }
+
+    private enum State
+    {
+        Attack,
+        Idle,
+    }
+    private FiniteStateMachine<State> finiteStateMachine;
+}
---
+++ Assets/Scripts/FiniteStateMachine.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class FiniteStateMachine<TState> : IFiniteStateMachine where TState : Enum
+{
+    public TState State
+    {
+        get
+        {
+            return state;
+        }
+        set
+        {
+            CallStateExit();
+            state = value;
+            CallStateEnter();
+        }
+    }
+    private TState state;
+
+    public FiniteStateMachine(MonoBehaviour monoBehaviour, TState initialState)
+    {
+        this.monoBehaviour = monoBehaviour;
+        state = initialState;
+        CacheMethod();
+        CallStateEnter();
+    }
+
+    private void CacheMethod()
+    {
+        var methodInfos = monoBehaviour.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        foreach (var methodInfo in methodInfos)
+        {
+            foreach (TState state in Enum.GetValues(typeof(TState)))
+            {
+                if (methodInfo.Name == "On" + state + "StateEnter")
+                {
+                    if (methodInfo.ReturnType == typeof(IEnumerator))
+                    {
+                        enterFuncs[state] = (Func<IEnumerator>)Delegate.CreateDelegate(typeof(Func<IEnumerator>), monoBehaviour, methodInfo);
+                    }
+                    else
+                    {
+                        enterActions[state] = (Action)Delegate.CreateDelegate(typeof(Action), monoBehaviour, methodInfo);
+                    }
+                }
+                else if (methodInfo.Name == "On" + state + "StateUpdate")
+                {
+                    updateActions[state] = (Action)Delegate.CreateDelegate(typeof(Action), monoBehaviour, methodInfo);
+                }
+                else if (methodInfo.Name == "On" + state + "StateExit")
+                {
+                    exitActions[state] = (Action)Delegate.CreateDelegate(typeof(Action), monoBehaviour, methodInfo);
+                }
+            }
+        }
+    }
+
+    public void Update()
+    {
+        CallStateUpdate();
+    }
+
+    private void CallStateEnter()
+    {
+        if (enterFuncs.TryGetValue(state, out Func<IEnumerator> coroutine))
+        {
+            monoBehaviour.StartCoroutine(coroutine());
+        }
+        else if (enterActions.TryGetValue(state, out Action action))
+        {
+            action();
+        }
+    }
+
+    private void CallStateUpdate()
+    {
+        if (updateActions.TryGetValue(state, out Action action))
+        {
+            action();
+        }
+    }
+
+    private void CallStateExit()
+    {
+        if (exitActions.TryGetValue(state, out Action action))
+        {
+            action();
+        }
+    }
+
+    private MonoBehaviour monoBehaviour;
+    private Dictionary<TState, Action> enterActions = new Dictionary<TState, Action>();
+    private Dictionary<TState, Action> updateActions = new Dictionary<TState, Action>();
+    private Dictionary<TState, Action> exitActions = new Dictionary<TState, Action>();
+    private Dictionary<TState, Func<IEnumerator>> enterFuncs = new Dictionary<TState, Func<IEnumerator>>();
+}
